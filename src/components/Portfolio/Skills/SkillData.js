66b358733afabc521e5bd05baca1a4fad7cfac19
khoatv2 <khoatv@technixo.com>
2023-03-11 00:51:02 +0700

    Add profile skill 2023

--- src/components/Portfolio/Skills/SkillData.js
+++ src/components/Portfolio/Skills/SkillData.js
@@ -1,203 +1,235 @@
-import csharp from '../../../assets/img/skills/languages/csharp.png';
-import js from '../../../assets/img/skills/languages/js.png';
-import sql from '../../../assets/img/skills/languages/sql.png';
-import css from '../../../assets/img/skills/languages/css.png';
-import scss from '../../../assets/img/skills/languages/scss.svg';
-import html from '../../../assets/img/skills/languages/html.png';
-import vbdotnet from '../../../assets/img/skills/languages/vbdotnet.png';
+import csharp from "../../../assets/img/skills/languages/csharp.png";
+import js from "../../../assets/img/skills/languages/js.png";
+import ts from "../../../assets/img/skills/languages/ts.png";
+import sql from "../../../assets/img/skills/languages/sql.png";
+import css from "../../../assets/img/skills/languages/css.png";
+import scss from "../../../assets/img/skills/languages/scss.svg";
+import html from "../../../assets/img/skills/languages/html.png";
+import vbdotnet from "../../../assets/img/skills/languages/vbdotnet.png";
 
-import aws from '../../../assets/img/skills/tools_framework/aws.svg';
-import bootstrap from '../../../assets/img/skills/tools_framework/bootstrap.svg';
-import expressjs from '../../../assets/img/skills/tools_framework/expressjs.svg';
-import mongodb from '../../../assets/img/skills/tools_framework/mongodb.svg';
-import mysql from '../../../assets/img/skills/tools_framework/mysql.svg';
-import nodejs from '../../../assets/img/skills/tools_framework/nodejs.png';
-import postgresql from '../../../assets/img/skills/tools_framework/postgresql.svg';
-import reactjs from '../../../assets/img/skills/tools_framework/reactjs.svg';
-import redux from '../../../assets/img/skills/tools_framework/redux.svg';
+import aws from "../../../assets/img/skills/tools_framework/aws.svg";
+import bootstrap from "../../../assets/img/skills/tools_framework/bootstrap.svg";
+import expressjs from "../../../assets/img/skills/tools_framework/expressjs.svg";
+import mongodb from "../../../assets/img/skills/tools_framework/mongodb.svg";
+import mysql from "../../../assets/img/skills/tools_framework/mysql.svg";
+import nextjs from "../../../assets/img/skills/tools_framework/nextjs.png";
+import chakra from "../../../assets/img/skills/tools_framework/chakra.png";
+import tailwind from "../../../assets/img/skills/tools_framework/tailwind.png";
+import nodejs from "../../../assets/img/skills/tools_framework/nodejs.png";
+import postgresql from "../../../assets/img/skills/tools_framework/postgresql.svg";
+import reactjs from "../../../assets/img/skills/tools_framework/reactjs.svg";
+import redux from "../../../assets/img/skills/tools_framework/redux.svg";
 
-import git from '../../../assets/img/skills/others/git.svg';
-import googledriveapi from '../../../assets/img/skills/others/googledriveapi.svg';
-import msoffice from '../../../assets/img/skills/others/msoffice.png';
-import npm from '../../../assets/img/skills/others/npm.svg';
-import postman from '../../../assets/img/skills/others/postman.png';
-import ps from '../../../assets/img/skills/others/ps.svg';
-import vscode from '../../../assets/img/skills/others/vscode.svg';
-import visualstudio from '../../../assets/img/skills/others/visualstudio.svg';
+import git from "../../../assets/img/skills/others/git.svg";
+import googledriveapi from "../../../assets/img/skills/others/googledriveapi.svg";
+import msoffice from "../../../assets/img/skills/others/msoffice.png";
+import npm from "../../../assets/img/skills/others/npm.svg";
+import postman from "../../../assets/img/skills/others/postman.png";
+import ps from "../../../assets/img/skills/others/ps.svg";
+import vscode from "../../../assets/img/skills/others/vscode.svg";
+import visualstudio from "../../../assets/img/skills/others/visualstudio.svg";
 
 export const languageData = [
-    {
-        id: 1,
-        name: 'C#',
-        img: csharp,
-        colorBadge: '#999',
-        colorEntry: '#60a5fa',
-    },
-    {
-        id: 2,
-        name: 'JavaScript',
-        img: js,
-        colorBadge: '#555',
-        colorEntry: '#84cc16',
-    },
-    {
-        id: 3,
-        name: 'SQL',
-        img: sql,
-        colorBadge: '#64748b',
-        colorEntry: '#00f9f2',
-    },
-    {
-        id: 4,
-        name: 'HTML',
-        img: html,
-        colorBadge: '#555',
-        colorEntry: '#ffa500',
-    },
-    {
-        id: 5,
-        name: 'CSS',
-        img: css,
-        colorBadge: '#555',
-        colorEntry: '#d3d3d3',
-    },
-    {
-        id: 6,
-        name: 'SCSS',
-        img: scss,
-        colorBadge: '#2c3648',
-        colorEntry: '#CD6799',
-    },
-    {
-        id: 7,
-        name: 'VB',
-        img: vbdotnet,
-        colorBadge: '#777',
-        colorEntry: '#00f9f2',
-    },
+  {
+    id: 1,
+    name: "C#",
+    img: csharp,
+    colorBadge: "#999",
+    colorEntry: "#60a5fa",
+  },
+  {
+    id: 2,
+    name: "JavaScript",
+    img: js,
+    colorBadge: "#555",
+    colorEntry: "#84cc16",
+  },
+  {
+    id: 4,
+    name: "SQL",
+    img: sql,
+    colorBadge: "#64748b",
+    colorEntry: "#00f9f2",
+  },
+  {
+    id: 5,
+    name: "HTML",
+    img: html,
+    colorBadge: "#555",
+    colorEntry: "#ffa500",
+  },
+  {
+    id: 6,
+    name: "CSS",
+    img: css,
+    colorBadge: "#555",
+    colorEntry: "#d3d3d3",
+  },
+  {
+    id: 7,
+    name: "SCSS",
+    img: scss,
+    colorBadge: "#2c3648",
+    colorEntry: "#CD6799",
+  },
+  {
+    id: 8,
+    name: "VB",
+    img: vbdotnet,
+    colorBadge: "#777",
+    colorEntry: "#00f9f2",
+  },
+  {
+    id: 3,
+    name: "TypeScript",
+    img: ts,
+    colorBadge: "#999",
+    colorEntry: "#f2f2f2",
+  },
 ];
 export const frameworkData = [
-    {
-        id: 1,
-        name: 'Reactjs',
-        img: reactjs,
-        colorBadge: '#555',
-        colorEntry: '#61dbfb',
-    },
-    {
-        id: 2,
-        name: 'Redux',
-        img: redux,
-        colorBadge: '#61dbfb',
-        colorEntry: '#734ead',
-    },
-    {
-        id: 3,
-        name: 'Nodejs',
-        img: nodejs,
-        colorBadge: '#313131',
-        colorEntry: '#78b65e',
-    },
-    {
-        id: 4,
-        name: 'Express',
-        img: expressjs,
-        colorBadge: '#75a76f',
-        colorEntry: '#9bcd28',
-    },
-    {
-        id: 5,
-        name: 'AWS',
-        img: aws,
-        colorBadge: '#fff',
-        colorEntry: '#ff9a00',
-    },
-    {
-        id: 6,
-        name: 'Bootstrap',
-        img: bootstrap,
-        colorBadge: '#999',
-        colorEntry: '#912eed',
-    },
+  {
+    id: 1,
+    name: "Reactjs",
+    img: reactjs,
+    colorBadge: "#555",
+    colorEntry: "#61dbfb",
+  },
+  {
+    id: 2,
+    name: "Redux",
+    img: redux,
+    colorBadge: "#61dbfb",
+    colorEntry: "#734ead",
+  },
+  {
+    id: 3,
+    name: "Nodejs",
+    img: nodejs,
+    colorBadge: "#313131",
+    colorEntry: "#78b65e",
+  },
+  {
+    id: 4,
+    name: "Express",
+    img: expressjs,
+    colorBadge: "#75a76f",
+    colorEntry: "#9bcd28",
+  },
+  {
+    id: 5,
+    name: "AWS",
+    img: aws,
+    colorBadge: "#fff",
+    colorEntry: "#ff9a00",
+  },
+  {
+    id: 6,
+    name: "Bootstrap",
+    img: bootstrap,
+    colorBadge: "#999",
+    colorEntry: "#912eed",
+  },
+  {
+    id: 7,
+    name: "NextJS",
+    img: nextjs,
+    colorBadge: "#ffffff",
+    colorEntry: "#999",
+  },
+  {
+    id: 8,
+    name: "Chakra UI",
+    img: chakra,
+    colorBadge: "#6cdda9",
+    colorEntry: "#ffffff",
+  },
+  {
+    id: 9,
+    name: "Tailwind CSS",
+    img: tailwind,
+    colorBadge: "#ffffff",
+    colorEntry: "#4db4b3",
+  },
 ];
 export const databaseData = [
-    {
-        id: 1,
-        name: 'MySQL',
-        img: mysql,
-        colorBadge: '#b4b4b4',
-        colorEntry: '#ffa518',
-    },
-    {
-        id: 2,
-        name: 'MongoDB',
-        img: mongodb,
-        colorBadge: '#d2d2d2',
-        colorEntry: '#07ac4f',
-    },
-    {
-        id: 3,
-        name: 'PostgresSQL',
-        img: postgresql,
-        colorBadge: '#999',
-        colorEntry: '#2f6792',
-    },
+  {
+    id: 1,
+    name: "MySQL",
+    img: mysql,
+    colorBadge: "#b4b4b4",
+    colorEntry: "#ffa518",
+  },
+  {
+    id: 2,
+    name: "MongoDB",
+    img: mongodb,
+    colorBadge: "#d2d2d2",
+    colorEntry: "#07ac4f",
+  },
+  {
+    id: 3,
+    name: "PostgresSQL",
+    img: postgresql,
+    colorBadge: "#999",
+    colorEntry: "#2f6792",
+  },
 ];
 export const relevantData = [
-    {
-        id: 1,
-        name: 'Git',
-        img: git,
-        colorBadge: '#3f2c00',
-        colorEntry: '#f05030',
-    },
-    {
-        id: 2,
-        name: 'npm',
-        img: npm,
-        colorBadge: '#d3d3d3',
-        colorEntry: '#cc3534',
-    },
-    {
-        id: 3,
-        name: 'Drive API',
-        img: googledriveapi,
-        colorBadge: '#d3d3d3',
-        colorEntry: '#f5c954',
-    },
-    {
-        id: 4,
-        name: 'VS Code',
-        img: vscode,
-        colorBadge: '#d3d3d3',
-        colorEntry: '#45aff3',
-    },
-    {
-        id: 5,
-        name: 'Visual Studio',
-        img: visualstudio,
-        colorBadge: '#d3d3d3',
-        colorEntry: '#0173c1',
-    },
-    {
-        id: 6,
-        name: 'Postman',
-        img: postman,
-        colorBadge: '#555',
-        colorEntry: '#f26634',
-    },
-    {
-        id: 7,
-        name: 'Photoshop',
-        img: ps,
-        colorBadge: '#999',
-        colorEntry: '#449ff2',
-    },
-    {
-        id: 8,
-        name: 'MS Office',
-        img: msoffice,
-        colorBadge: '#666',
-        colorEntry: '#df3900',
-    },
+  {
+    id: 1,
+    name: "Git",
+    img: git,
+    colorBadge: "#3f2c00",
+    colorEntry: "#f05030",
+  },
+  {
+    id: 2,
+    name: "npm",
+    img: npm,
+    colorBadge: "#d3d3d3",
+    colorEntry: "#cc3534",
+  },
+  {
+    id: 3,
+    name: "Drive API",
+    img: googledriveapi,
+    colorBadge: "#d3d3d3",
+    colorEntry: "#f5c954",
+  },
+  {
+    id: 4,
+    name: "VS Code",
+    img: vscode,
+    colorBadge: "#d3d3d3",
+    colorEntry: "#45aff3",
+  },
+  {
+    id: 5,
+    name: "Visual Studio",
+    img: visualstudio,
+    colorBadge: "#d3d3d3",
+    colorEntry: "#0173c1",
+  },
+  {
+    id: 6,
+    name: "Postman",
+    img: postman,
+    colorBadge: "#555",
+    colorEntry: "#f26634",
+  },
+  {
+    id: 7,
+    name: "Photoshop",
+    img: ps,
+    colorBadge: "#999",
+    colorEntry: "#449ff2",
+  },
+  {
+    id: 8,
+    name: "MS Office",
+    img: msoffice,
+    colorBadge: "#666",
+    colorEntry: "#df3900",
+  },
 ];
