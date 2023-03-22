99ac0bfbd156f9668b420362cbaf00b4bf9d4832
khoatv2 <khoatv@technixo.com>
2023-03-23 00:36:49 +0700

    Modify about 2023

--- src/components/Portfolio/Home/Home.js
+++ src/components/Portfolio/Home/Home.js
@@ -1,54 +1,54 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { FormattedMessage } from 'react-intl';
+import React from "react";
+import { connect } from "react-redux";
+import { FormattedMessage } from "react-intl";
 
-import './Home.scss';
-import Aboutme from './Aboutme/Aboutme';
+import "./Home.scss";
+import Aboutme from "./Aboutme/Aboutme";
 
 const Home = (props) => {
-    const avatar =
-        'https://raw.githubusercontent.com/kanechan25/portfolio/main/src/assets/img/avatar/portrait_2015.jpg';
+  const avatar =
+    "https://raw.githubusercontent.com/kanechan25/portfolio/main/src/assets/img/about/avatars.jpg";
 
-    return (
-        <div className="homeContainer container">
-            <div className="row">
-                <div className="leftContainer col-md-6 col-12">
-                    <Aboutme welcome={true} portrait={avatar} />
-                    <div className="descContainer">
-                        <p>
-                            &#183; <FormattedMessage id="home.desc1" />
-                        </p>
-                        <p>
-                            &#183; <FormattedMessage id="home.desc2" />
-                        </p>
-                        <p>
-                            &#183; <FormattedMessage id="home.desc3" />
-                        </p>
-                    </div>
-                </div>
-                <div className="rightContainer col-md-6 col-12">
-                    <iframe
-                        id="introduceVideo"
-                        className="introduceVideo"
-                        src="https://www.youtube.com/embed/K1HJgS9wZB4"
-                        title="YouTube video player"
-                        frameBorder="0"
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                        allowFullScreen
-                    ></iframe>
-                </div>
-            </div>
+  return (
+    <div className="homeContainer container">
+      <div className="row">
+        <div className="leftContainer col-md-6 col-12">
+          <Aboutme welcome={true} portrait={avatar} />
+          <div className="descContainer">
+            <p>
+              &#183; <FormattedMessage id="home.desc1" />
+            </p>
+            <p>
+              &#183; <FormattedMessage id="home.desc2" />
+            </p>
+            <p>
+              &#183; <FormattedMessage id="home.desc3" />
+            </p>
+          </div>
         </div>
-    );
+        <div className="rightContainer col-md-6 col-12">
+          <iframe
+            id="introduceVideo"
+            className="introduceVideo"
+            src="https://www.youtube.com/embed/K1HJgS9wZB4"
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {
-        language: state.language,
-    };
+  return {
+    language: state.language,
+  };
 };
 
 const mapDispatchToProps = (dispatch) => {
-    return {};
+  return {};
 };
 export default connect(mapStateToProps, mapDispatchToProps)(Home);
